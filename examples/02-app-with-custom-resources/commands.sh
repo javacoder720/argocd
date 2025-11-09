7ff70ecb463845f1745ea923d02a65dad5bcaa3f
Gates Wang <gateswang@gmail.com>
2025-11-09 02:10:34 -0600

    turn off autosync

--- examples/02-app-with-custom-resources/commands.sh
+++ examples/02-app-with-custom-resources/commands.sh
@@ -7,7 +7,7 @@ helm install argocd argo/argo-cd \
     --set server.service.type=LoadBalancer
 kubectl port-forward service/argocd-server -n argocd 8080:443
 
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 argocd login localhost:8080 --username admin --password $ARGOCD_PASSWORD --insecure
 
 
